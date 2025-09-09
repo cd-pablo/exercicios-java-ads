9985c5000f7264906d4b272443737724e89da287
cd-pablo <pablobaiocoribad@gmail.com>
2025-09-08 22:12:09 -0300

    exerc

--- programacao-2/4-Vetores/src/Vetores/ex11.java
+++ programacao-2/4-Vetores/src/Vetores/ex11.java
@@ -1,7 +1,10 @@
 package src.Vetores;
 
+import java.util.Scanner;
+
 public class ex11 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
