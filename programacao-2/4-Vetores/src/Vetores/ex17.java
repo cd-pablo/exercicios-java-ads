db751af7cceb863b116484308985d9be666c26d9
cd-pablo <pablobaiocoribad@gmail.com>
2025-09-29 20:42:23 -0300

    Implementa ex17: lógica de votação com vetores

--- programacao-2/4-Vetores/src/Vetores/ex17.java
+++ programacao-2/4-Vetores/src/Vetores/ex17.java
@@ -6,16 +6,28 @@ public class ex17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+                String[] nomes = new String[3];
+                int[] votos = new int[3];
+
+                for (int i = 0; i < 3; i++) {
+                    System.out.print("Digite o nome do candidato " + (i + 1) + ": ");
+                    nomes[i] = scanner.nextLine();
+
+                    System.out.print("Digite a quantidade de votos de " + nomes[i] + ": ");
+                    votos[i] = scanner.nextInt();
+                    scanner.nextLine();
+                }
 
 
+                int indiceVencedor = 0;
+                for (int i = 1; i < 3; i++) {
+                    if (votos[i] > votos[indiceVencedor]) {
+                        indiceVencedor = i;
+                    }
+                }
 
+                System.out.println("O candidato vencedor é: " + nomes[indiceVencedor]);
 
-
-
-
-
-
-
-
-    }
-}
+                scanner.close();
+            }
+        }
