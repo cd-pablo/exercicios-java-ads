10e036ed0cae4edf1fba0101b971bfb5e382c082
cd-pablo <pablobaiocoribad@gmail.com>
2025-09-30 22:11:24 -0300

    coisas

--- programacao-2/5-Matrizes/src/Matrizes/ex2.java
+++ programacao-2/5-Matrizes/src/Matrizes/ex2.java
@@ -1,18 +1,17 @@
 package src.Matrizes;
-import java.util.Scanner;
 
 public class ex2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         int[][] matriz = new int[5][5];
-        int diagonal = 0;
 
-        System.out.println("Digite os valores da matriz 5x5");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                System.out.print("Elemento [" + i + "][" + j + "]: ");
-                matriz[i][j] = scanner.nextInt();
+                if (i == j) {
+                    matriz[i][j] = 1;
+                } else {
+                    matriz[i][j] = 0;
+                }
             }
         }
 
@@ -20,11 +19,10 @@ public class ex2 {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print(matriz[i][j] + " ");
-
             }
             System.out.println();
-
-
         }
+
+
     }
 }
