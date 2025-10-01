0da63105f56b52cfe75d2141852e9b8c222d2959
cd-pablo <pablobaiocoribad@gmail.com>
2025-09-30 22:16:57 -0300

    coisas

--- programacao-2/5-Matrizes/src/Matrizes/ex2.java
+++ programacao-2/5-Matrizes/src/Matrizes/ex2.java
@@ -5,9 +5,10 @@ public class ex2 {
 
         int[][] matriz = new int[5][5];
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (i == j) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+
+                if (matriz[i] == matriz[j]) {
                     matriz[i][j] = 1;
                 } else {
                     matriz[i][j] = 0;
@@ -16,8 +17,8 @@ public class ex2 {
         }
 
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
                 System.out.print(matriz[i][j] + " ");
             }
             System.out.println();
