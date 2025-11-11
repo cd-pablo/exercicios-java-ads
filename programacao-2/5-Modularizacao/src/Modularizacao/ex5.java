0d076ee9e5d4b15ffa9c61184c5dd9b6c7ce0dae
cd-pablo <pablobaiocoribad@gmail.com>
2025-11-11 20:08:57 -0300

    .

--- programacao-2/5-Modularizacao/src/Modularizacao/ex5.java
+++ programacao-2/5-Modularizacao/src/Modularizacao/ex5.java
@@ -2,6 +2,37 @@ package src.Modularizacao;
 import java.util.Scanner;
 
 public class ex5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha a entrada:");
+        System.out.println("1 - Celsius");
+        System.out.println("2 - Fahrenheit");
+        int op = scanner.nextInt();
+
+        double tempConvert;
+
+        switch (op) {
+            case 1:
+                System.out.print("Digite em Celsius: ");
+                double celsius = scanner.nextDouble();
+                tempConvert = CpraF(celsius);
+                System.out.println(celsius + "°C é igual a " + tempConvert + "°F");
+                break;
+
+            case 2:
+                System.out.print("Digite em Fahrenheit: ");
+                double fahrenheit = scanner.nextDouble();
+                tempConvert = FpraC(fahrenheit);
+                System.out.println(fahrenheit + "°F é igual a " + tempConvert + "°C");
+                break;
+
+            default:
+                System.out.println("inválida");
+                break;
+        }
+
+    }
 
     public static double CpraF(double celsius) {
         return (9.0 * celsius / 5.0) + 32;
@@ -11,38 +42,5 @@ public class ex5 {
         return 5.0 * (fahrenheit - 32) / 9.0;
     }
 
-
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Escolha a entrada:");
-        System.out.println("1 - Temperatura em Celsius");
-        System.out.println("2 - Temperatura em Fahrenheit");
-        int escolha = scanner.nextInt();
-
-        double tempConvert;
-
-        switch (escolha) {
-            case 1:
-                System.out.print("Digite a temperatura em Celsius: ");
-                double celsius = scanner.nextDouble();
-                tempConvert = CpraF(celsius);
-                System.out.println(celsius + "°C é igual a " + tempConvert + "°F");
-                break;
-
-            case 2:
-                System.out.print("Digite a temperatura em Fahrenheit: ");
-                double fahrenheit = scanner.nextDouble();
-                tempConvert = FpraC(fahrenheit);
-                System.out.println(fahrenheit + "°F é igual a " + tempConvert + "°C");
-                break;
-
-            default:
-                System.out.println("Opção inválida!");
-                break;
-        }
-
-    }
 
 }
